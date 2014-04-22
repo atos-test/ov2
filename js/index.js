191e6adb8a044ef41a2edb40db5746ec8832f64b
test-atos <a.gomezmart@hotmail.com>
2014-04-22 13:33:16 +0200

    handleOrientation

--- js/index.js
+++ js/index.js
@@ -29,46 +29,72 @@ var app = {
 
         $("#containerDetalle1").hide();
 
-        /*Si venimos de detalle, hacemos un scroll al inicio de la lista*/
+        //Si estoy en LANDSCAPE cargo un template para detalle
+        if(window.orientation == -90 || window.orientation == 90) {
+            console.log("Estoy en landscape");
+
+            //Cargamos el template de la lista
+            self.templateLista();
+
+            //Cargamos el template del detalle LANDSCAPE
+            //self.templateDetalleLandscape();
+
+            //Calculamos la altura de la imagen en función del tipo de pantalla (480x800, 320x480..)
+            //También se calcula el tamaño de la letra y el punto medio donde ubicar el boton info
+            self.calcularAlturaImgLista();
+
+        }
+        //Si estoy en PORTRAIT cargo los templates hechos anteriormente
+        else{
+
+            self.products = app.hojaProductos.chojaProductos.Producto.cProducto;
+            
+            //Cargamos el template de la lista
+            self.templateLista();
+
+            //Cargamos el template del detalle PORTRAIT
+            self.templateDetallePortrait();
 
 
-        self.products = app.hojaProductos.chojaProductos.Producto.cProducto;
+            var alturaDocument = $( document ).height();
+            //Calculamos la altura de la imagen en función del tipo de pantalla (480x800, 320x480..)
+            //También se calcula el tamaño de la letra y el punto medio donde ubicar el boton info
+            self.calcularAlturaImgLista();
+            
+            /* Calculamos el número de bloques que se van a visualizar*/
+            var alturaDetalle = 403;
+            var nbloques = Math.floor((alturaDocument-48)/alturaDetalle);
+            
+            /* Para la altura adaptable de los bloques de detalle de producto */
+            console.log("alturaDocument: ", alturaDocument);
+            console.log("nbloques: ", nbloques);
+            
+            var alturaDetalle = (alturaDocument-48)/nbloques;
+            $(".detalle-block").height(alturaDetalle);
+            console.log("alturaDetalle: ", alturaDetalle);
+            $(".texto-servicio").height(alturaDetalle-88-154-83);
+        }
+    },
+
+    templateLista: function(){
+        var self = app;
         //Template de lista
         var template = _.template($('#productList-template').html(), {productList : self.products});
         $('#productosList').html(template);
         self.refreshGroups(self.products.length);
         $("#productosList").listview().listview('refresh');
+    },
 
+    templateDetallePortrait: function(){
+        var self = app;
         //Template de detalle
         var templateDetalle = _.template($('#productDetail-template').html(), {productDetail : self.products});
         $('#detalle').html(templateDetalle);
-
-
-        var alturaDocument = $( document ).height();
-        //Calculamos la altura de la imagen en función del tipo de pantalla (480x800, 320x480..)
-        //También se calcula el tamaño de la letra y el punto medio donde ubicar el boton info
-        self.calcularAlturaImgLista();
-		
-		/* Calculamos el número de bloques que se van a visualizar*/
-		var alturaDetalle = 403;
-		var nbloques = Math.floor((alturaDocument-48)/alturaDetalle);
-		
-		/* Para la altura adaptable de los bloques de detalle de producto */
-		console.log("alturaDocument: ", alturaDocument);
-		console.log("nbloques: ", nbloques);
-		
-		var alturaDetalle = (alturaDocument-48)/nbloques;
-		$(".detalle-block").height(alturaDetalle);
-		console.log("alturaDetalle: ", alturaDetalle);
-		$(".texto-servicio").height(alturaDetalle-88-154-83);
-        
     },
 
     refreshGroups: function(total){
-        for (var i = 0; i<total; i++) {
-            var id= "#myIdGroup"+i;
-            $(id).controlgroup().controlgroup("refresh");
-        };
+        var cg = document.getElementsByName("controlGroupLista");
+        $(cg).controlgroup().controlgroup("refresh");
     },
 
     calcularAlturaImgLista: function(){
@@ -89,7 +115,7 @@ var app = {
         
 
         /*Calculamos el tamaño de la letra en función del alto/ancho de la imagen*/
-        var tamNombreProducto = anchoImg/100 + 0.1 + "em";
+        var tamNombreProducto = anchoImg/100 + 0.05 + "em";
         var tamPrecioProducto = anchoImg/100 + "em";
 
         $(".nombreProducto").css({ 'font-size': tamNombreProducto});
@@ -101,11 +127,6 @@ var app = {
 
         var alturaMediaInfo = anchoImg/2 - 14 + "px";
         $(".btnInfo").css({ 'margin-top': alturaMediaInfo});
-
-        console.log("AnchoUL-->", anchoUL);
-        console.log("anchoControlGroupIzq-->", anchoControlGroupIzq);
-        console.log("anchoControlGroupDer-->", anchoControlGroupDer);
-        console.log("anchoImg-->", anchoImg);
     },
 
     setWidthImg: function(ancho){
@@ -251,9 +272,9 @@ var app = {
             });
         });
 
-        /*window.onorientationchange = function() {
+        window.onorientationchange = function() {
             self.refreshView();
-        }*/
+        }
 
         document.addEventListener('deviceready', function(){
             console.log("Received Event: deviceready");
